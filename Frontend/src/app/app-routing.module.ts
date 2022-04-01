b2cc45a9cd46bfa6c8cece2e0fb3db92fe913d94
Darwin Gonzalez <8992534+DwGonzalez@users.noreply.github.com>
2022-04-01 11:04:49 -0400

    UI Config
    Added Initial UI Config
    Added Bootstrap

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BookListComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'books'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
