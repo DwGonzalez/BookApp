a0d05ac0bdba3e805fa155a989c41546f1a8083e
Darwin Gonzalez <8992534+DwGonzalez@users.noreply.github.com>
2022-04-01 09:01:05 -0400

    ➕ Book and operation models. ➕ Service Interface

---
+++ Backend/ExternalServices/Interface/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ExternalServices.Interface
+{
+    public interface IBookService
+    {
+        Task CreateBook(Book book);
+        Task UpdateBook(int id, Book book);
+        Task GetAllBooks();
+        Task GetBook(int id);
+        Task DeleteBook(int id);
+    }
+}
